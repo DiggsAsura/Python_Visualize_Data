627d075b0b1349facc204854b3a015618ecdf2f4
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-13 12:34:12 +0200

    3_1_7_4-Stacked_Bars.py work in progress

---
+++ 3_1_7_4-Stacked_Bars.py
@@ -0,0 +1,14 @@
+# 3. Graphing in Python
+# 1. Graphing in Python: Matplotlib
+# 7. Recreate Graphs using Matplotlib
+# 4. Stacked Bars
+
+'''
+Now, we are going to look at the chart called stacked-bars.png. This graph 
+displays the breakdown of students who got As, Bs, Cs, Ds and Fs in each unit.
+
+The data you will need to recreate this chart is in the lists As, Bs, Cs, Ds, Fs
+and unit_topics
+
+Save your recreated chart to a file called my_stacked_bar.png
+'''

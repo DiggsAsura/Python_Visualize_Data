cdf1cc83eeba64ce3d8eeb45f7e007a078c3e4b5
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-14 07:05:00 +0200

    3_1_7_4-Stacked_Bars.py work in progress

--- 3_1_7_4-Stacked_Bars.py
+++ 3_1_7_4-Stacked_Bars.py
@@ -12,3 +12,27 @@ and unit_topics
 
 Save your recreated chart to a file called my_stacked_bar.png
 '''
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+As = [6, 3, 4, 3, 5]
+Bs = [8, 12, 8, 9, 10]
+Cs = [13, 12, 15, 13, 14]
+Ds = [2, 3, 3, 2, 1]
+Fs = [1, 0, 0, 3, 0]
+
+x = range(5)
+c_bottom = np.add(As, Bs)
+d_bottom = np.add(c_bottom, Cs)
+f_bottom = np.add(d_bottom, Ds)
+
+plt.figure(figsize=(10,8))
+
+#ALWAYS remember, needs both X and Y (i forget what to put in lol)
+plt.bar(x, As)
+plt.bar(x, Bs, bottom=)
+
+
+plt.show()
